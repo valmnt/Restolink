fe7be76396f03c41400bba810b4963c65bb9a8cc
Valmnt <valentin.mont@ynov.com>
2020-03-24 14:25:51 +0100

    restaurants controller

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -37,7 +37,7 @@ class RestaurantController extends AbstractController
     /**
      * @Route("/{id}", name="restaurant_unique")
      */
-    public function unique(Restaurant $restaurant, RestaurantRepository $restaurantRepository)
+    public function unique(Restaurant $restaurant)
     {
         $plats = $restaurant->getPlats();
         return $this->render('restaurant/restaurant-plat.html.twig', [
