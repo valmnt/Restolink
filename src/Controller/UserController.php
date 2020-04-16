4d608297a4e185273902b2d9eda3e18c33ca5695
Valmnt <valentin.mont@ynov.com>
2020-04-16 12:45:54 +0200

    try update amount

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\EditUserType;
+use App\Form\WalletType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -66,5 +68,29 @@ class UserController extends AbstractController
             $this->entityManagerInterface->flush();
             return $this->render('index/index.html.twig');
         }
+    }
+
+    /**
+     * @Route("/update_wallet/{id}", name="update_wallet")
+     */
+    public function updateWallet(Request $request, User $user)
+    {
+        $form = $this->createForm(WalletType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $user->getPassword();
+            $userData = new User();
+            $userData = $form->getData();
+            $formPassword = $userData->getPassword();
+
+            if ($password === $formPassword) {
+                $this->entityManagerInterface->flush();
+                $role = $user->getRoles();
+                return $this->render('user/index.html.twig', ['user' => $user, 'role' => $role]);
+            }
+        }
+
+        return $this->render('user/wallet.html.twig', ['form' => $form->createView()]);
     }
 }
