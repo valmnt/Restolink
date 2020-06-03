811ed1e7e0176f708d30cadca358cbd3718ed03a
kevin <kevgym69@gmail.com>
2020-06-03 09:09:14 +0200

    small fix

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,6 +19,7 @@ class UserType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('solde')
+            ->add('Submit', SubmitType::class)
         ;
     }
 
